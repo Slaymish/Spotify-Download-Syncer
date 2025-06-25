7ca227178300fc7abe34b1b245a8a906f094d7ef
Hamish Burke <hamishapps@gmail.com>
2025-06-25 12:34:52 +1200

    Refactor test_soulseek function to accept searcher parameter and improve song processing logic

--- soulseek_test.py
+++ soulseek_test.py
@@ -1,37 +1,50 @@
 import os.path
+from spotify_syncer.torrent_searchers import SoulseekSearcher
 
 
-def test_soulseek(song):
-	pass
+def test_soulseek(song,searcher):
+    # will be None if failed
+    file = searcher.search(song)
+    return file
 
 
 
 def main():
-	songs = [
-			'messy by oeahgou',
-			'song 2',
-			'song 3',
-			'song 4'
-			]
+    print("starting test....")
+    songs = [
+        'messy by oeahgou',
+        'song 2',
+        'song 3',
+        'song 4'
+    ]
 
-	test_passed = 0
+    test_passed = 0
+    searcher = SoulseekSearcher()
+    print("created searcher object")
 
-	for song in songs:
-		# will try and download song
-		test_soulseek(song)
+    for song in songs:
+        print(f"Processing {song}...")
 
-		# check if exists (like the .mp3)
-		exists = os.path.isfile(song)
+        # will try and download song
+        file = test_soulseek(song,searcher)
 
-		# if exists, incremented test_passed by 1
-		if exists:
-			test_passed += 1
-		else:
-			print(f"{song} failed to download")
+        print("Search finished")
+
+        # check if exists (like the .mp3)
+        if file:
+            exists = os.path.isfile(file)
+        else:
+            exists = False
+
+        # if exists, incremented test_passed by 1
+        if exists:
+            test_passed += 1
+        else:
+            print(f"{song} failed to download")
 
 
-	print("Tests finished")
-	print(f"{test_passed}/{len(songs)} passed")
+    print("Tests finished")
+    print(f"{test_passed}/{len(songs)} passed")
 
 
 
